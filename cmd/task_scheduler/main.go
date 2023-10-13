6917d86b312ac5c6c600a52ee59d90c544829053
DanilNaum <90374685+DanilNaum@users.noreply.github.com>
2023-10-13 18:29:08 +0300

    Изменена функция task doing, добавлена возможность изменения количесва потоков, путем изменения только 1 параметра. парсинг строки  вместе с отбором невалидных строк перенесен в функцию newtask, изменена логика работы ticker, теперь минутные и часовые задачи начинают выполняться в 00 секунд/минут

--- cmd/task_scheduler/main.go
+++ cmd/task_scheduler/main.go
@@ -15,21 +15,38 @@ import (
 	"time"
 )
 type Task struct{
+	task_type string
 	name string
 	min_time int
 	max_time int
 
 }
 func NewTask (a []string) *Task{
-	mi,_ := strconv.Atoi(a[1])
-	ma,_ := strconv.Atoi(a[2])
-	task := Task{name: a[0], min_time: mi, max_time: ma }
+	if len(a) != 5{
+			fmt.Println("Ошибка! Неверные аргументы функции, ожидается 3 аргумента, получено", len(a)-2)
+			return nil
+	}
+	mi,err := strconv.Atoi(a[3])
+	if err != nil {
+		fmt.Println("Ошибка! Неверные аргументы функции, 2 аргумент должен быть числом")
+		return nil
+	}
+	ma,err := strconv.Atoi(a[4])
+	if err != nil {
+		fmt.Println("Ошибка! Неверные аргументы функции, 3 аргумент должен быть числом")
+		return nil
+	}
+	if mi>ma{
+		fmt.Println("Ошибка! Неверные аргументы функции, минимальное время выполнения функции превосходит максимальное")
+		return nil
+	}
+	task := Task{task_type: a[0],name: a[2], min_time: mi, max_time: ma }
 	return &task
 }
-type Tasks struct {
+type Tasks struct { // переделать остальное
     secondly []*Task
-    minutely []string
-    hourly    []string
+    minutely []*Task
+    hourly    []*Task
 }
 
 func ReadData(fileName string, tasks *Tasks, mu *sync.Mutex){
@@ -47,17 +64,15 @@ func ReadData(fileName string, tasks *Tasks, mu *sync.Mutex){
 		var tmp_tasks Tasks
 
 		for s.Scan(){
-			if strings.Split(s.Text()," ")[0] == "secondly"{
-				
-				// tmp_tasks.secondly = append(tmp_tasks.secondly,  s.Text())
-				nextTask := NewTask(strings.Split(s.Text()," ")[2:])
-				tmp_tasks.secondly = append(tmp_tasks.secondly, nextTask)
-			
-			}else if strings.Split(s.Text()," ")[0] == "minutely"{
-				// minute_task_tmp = append(minute_task_tmp, s.Text())
-				tmp_tasks.minutely = append(tmp_tasks.minutely, s.Text())
-			}else if strings.Split(s.Text()," ")[0] == "hourly"{
-				tmp_tasks.hourly = append(tmp_tasks.hourly, s.Text())
+			nextTask := NewTask(strings.Split(s.Text()," "))
+			if nextTask != nil{
+				if nextTask.task_type == "secondly"{
+					tmp_tasks.secondly = append(tmp_tasks.secondly, nextTask)
+				}else if nextTask.task_type == "minutely"{
+					tmp_tasks.minutely = append(tmp_tasks.minutely, nextTask)
+				}else if nextTask.task_type == "hourly"{
+					tmp_tasks.hourly = append(tmp_tasks.hourly,nextTask)
+				}
 			}
 		}
 		mu.Lock()
@@ -89,11 +104,26 @@ func ReadData(fileName string, tasks *Tasks, mu *sync.Mutex){
 }
 
 
-func TaskMaker(task *Tasks, mu *sync.Mutex,data_channel chan string, data_channel2 chan *Task){
+func TaskMaker(task *Tasks, mu *sync.Mutex, data_channel chan *Task){
 // функция передачи задач на выполнение с определенной переодичностью
 	tickSecond := time.NewTicker(time.Second)
-	tickMinute := time.NewTicker(time.Minute)
-	tickHour := time.NewTicker(time.Hour)
+	tickMinute := time.NewTicker(2 * time.Hour)//временое значение, чтоб запиуить тикер с 00 секунд
+	tickHour := time.NewTicker(2 * time.Hour)
+	now := time.Now()
+    nextMinutelyTick := time.Date(now.Year(), now.Month(), now.Day(), now.Hour(), now.Minute()+1, 0, 0, now.Location())
+    nextHourlyTick := time.Date(now.Year(), now.Month(), now.Day(), now.Hour()+1, 0, 0, 0, now.Location())
+
+    timeToNextMinutelyTick := time.Until(nextMinutelyTick)
+    timeToNextHourlyTick := time.Until(nextHourlyTick)
+	go func(){
+		time.Sleep(timeToNextMinutelyTick)
+		tickMinute = time.NewTicker(time.Minute)
+	}()
+	go func(){
+		time.Sleep(timeToNextHourlyTick)
+		tickHour = time.NewTicker(time.Hour)
+	}()
+	
 	defer tickSecond.Stop() // освободим ресурсы, при завершении работы функции
 	defer tickMinute.Stop()
 	defer tickHour.Stop()
@@ -102,7 +132,7 @@ func TaskMaker(task *Tasks, mu *sync.Mutex,data_channel chan string, data_channe
 		case <-tickSecond.C:
 			mu.Lock()
 			for _,task :=range (task.secondly){
-				data_channel2<-task
+				data_channel<-task
 			}
 			mu.Unlock()
 		case <-tickMinute.C:
@@ -121,61 +151,37 @@ func TaskMaker(task *Tasks, mu *sync.Mutex,data_channel chan string, data_channe
 		}
 	}
 }
-
-func TaskDoing(numberOfSimultaneousRequests int, data_channel chan string, data_channel2 chan *Task  ){
-	thread := make(map[int] chan struct{})
-	for i := 0; i < numberOfSimultaneousRequests; i++ {
-		thread[i] = make(chan struct{},1)
-		thread[i] <-  struct{}{}
-		defer close(thread[i])
-	}
-
-	for task := range data_channel2{
-		// a := strings.Split(s," ")
+func worker(i int, data_channel chan *Task, wg *sync.WaitGroup ){
+	defer wg.Done()
+	for task := range data_channel{
+		// не понял как следить за закрытием канала
 		a := []string{task.name,strconv.Itoa(task.min_time),strconv.Itoa(task.max_time)}
-		go func(a []string){			
-			select {
-			case <-thread[0]:
-				testtasks.Wait(1,a)
-				thread[0] <-  struct{}{}
-			case <-thread[1]:
-				testtasks.Wait(2,a)
-				thread[1] <-  struct{}{}	
-			case <-thread[2]:
-				testtasks.Wait(3,a)
-				thread[2] <-  struct{}{}
-			case <-thread[3]:
-				testtasks.Wait(4,a)
-				thread[3] <-  struct{}{}	
-			case <-thread[4]:
-				testtasks.Wait(5,a)
-				thread[4] <-  struct{}{}	
-			}		
-		}(a)
+		testtasks.Wait(i,a)
 	}
-
+	
+}
+func TaskDoing(numberOfSimultaneousRequests int, data_channel chan *Task, wg *sync.WaitGroup ){
+	for i := 0; i < numberOfSimultaneousRequests; i++{
+		wg.Add(1)
+		go worker(i+1, data_channel, wg)
+	}
 }
 
+
+
 func main() {
-	// var second_task []string
-	// var minute_task []string
-	// var hour_task []string
 	var tasks Tasks
 	mu := new(sync.Mutex) 
-	
+	wg := new(sync.WaitGroup)
 	numberOfSimultaneousRequests := 5 //константа определенная условиями задания
-	data_channel := make(chan string) // канал используемый для передачи очереди задач
-	data_channel2 := make(chan *Task)
+	data_channel := make(chan *Task)
 	defer close(data_channel)
-	defer close(data_channel2)
-	// go ReadData("tasks.txt", &tasks ,&second_task, &minute_task, &hour_task, mu)
 	go ReadData("tasks.txt", &tasks, mu)
 
-	// go TaskMaker(&second_task, &minute_task, &hour_task, mu, data_channel)
-	go TaskMaker(&tasks, mu, data_channel, data_channel2)
+	go TaskMaker(&tasks, mu, data_channel)
 	
-	TaskDoing(numberOfSimultaneousRequests, data_channel, data_channel2)
-	// Наличие структуры на thread означает, что поток свободен и можно передать на него следующую задачу
+	TaskDoing(numberOfSimultaneousRequests, data_channel, wg)
 	
+	wg.Wait()
 
 }
